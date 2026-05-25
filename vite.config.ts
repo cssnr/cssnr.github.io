c5cdd5d5bec86e62be8fbc886de601fa615cb7c4
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-05-25 14:45:29 -0700

    Update Router, Dependencies and Apps (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import { copyFileSync } from 'node:fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/cssnr-vue/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html')
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
